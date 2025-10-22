caf8afbe324215d253a7814957d3f42a395bc014
wahab-ali5 <wahabali098445@gmail.com>
2025-10-22 16:27:07 +0500

    done

---
+++ app.js
@@ -0,0 +1,8 @@
+var nav = document.getElementById("nav");
+var footer = document.getElementById("footer");
+
+var footer2=document.getElementById("footer2");
+
+footer2.appendChild(footer);
+console.log(footer2);
+
